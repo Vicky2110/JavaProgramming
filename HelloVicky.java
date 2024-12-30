ef6c358e3cf152f233fdec4dd1ecf76f65a6c548
Vicky <kumvicky21@outlook.com>
2024-12-30 13:47:59 +0530

    First Change

---
+++ HelloVicky.java
@@ -0,0 +1,9 @@
+public class HelloVicky {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Vicky");
+        System.out.println("is good boy");
+        
+    }
+
+}
